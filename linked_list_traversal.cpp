01f0e546055632a7019fb7ae56bbc6fcbb35686e
Khalid <seokhalidmehmood@gmail.com>
2025-03-16 15:28:23 +0500

    traverse, Insertion At start , Middle , End Node

--- linked_list_traversal.cpp
+++ linked_list_traversal.cpp
@@ -12,16 +12,66 @@ class Node{
     }
 };
 
+// traverse of a Linked List 
 
 void trasvers(Node* head){
     while (head != nullptr)     
     {
         cout << head->data << " --> " ;
-        head = head->next ;
+        head = head->next ; 
     }
     cout << "Null" ;
-    
 }
+
+// Insert Node at the head 
+void insert_At_Head(Node*&head,int value_Of_New_Node){
+    Node* newNode = new Node(value_Of_New_Node);
+    newNode->next = head ; 
+    head = newNode ;
+}
+
+// insert Node at the end 
+void insert_At_End(Node* &Head , int value_At_End){
+    if (Head==NULL)
+    {
+        insert_At_Head(Head,value_At_End);
+    }
+    
+    Node* endNode = new Node (value_At_End);
+    Node* temp = Head ;
+
+    while (temp->next!=NULL)
+    {
+        temp = temp->next ;
+    }
+
+    temp->next=endNode;
+
+}
+
+// insert Node at the Middle 
+void insert_At_Middle(Node* &Head , int val , int position ){
+
+    if (position==1)
+    {
+        insert_At_Head(Head,val);
+        return ;
+    }
+    
+    Node* newNode = new Node (val);
+    Node* pre = Head ;
+    int count = 1 ;
+
+    while (count < (position-1))        
+    {
+        pre = pre->next ;
+        count ++ ;
+    }
+    newNode->next=pre->next;
+    pre->next=newNode;
+
+}
+
 
 int main (){
     Node* obj1 = new Node(99);
@@ -36,12 +86,18 @@ int main (){
 
     Node * head = obj1 ;
 
+    trasvers(head); 
+
+    insert_At_Head(head,100);
+    cout << endl ;
+    trasvers(head);
+
+    insert_At_End(head,9999);
+    cout << endl ;
     trasvers(head) ;
 
-
-    // cout << obj1->data << "\n" << obj1->next;
-    // cout << obj2->data << "\n" << obj2->next;
-    // cout << obj3->data << "\n" << obj3->next;
-    // cout << obj4->data << "\n" << obj4->next;
+    insert_At_Middle(head,888,2);
+    cout << endl ;
+    trasvers(head);
 
 }
